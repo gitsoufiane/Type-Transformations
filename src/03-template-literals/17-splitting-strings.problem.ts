1f0f9744bf873aa892d5e78e1bc34c0339002d02
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2024-06-16 18:49:55 -0700

    .

--- src/03-template-literals/17-splitting-strings.problem.ts
+++ src/03-template-literals/17-splitting-strings.problem.ts
@@ -6,7 +6,7 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type Path = "Users/John/Documents/notes.txt";
 
-type SplitPath = S<Path,'/'>;
+type SplitPath = S.Split<Path,'/'>;
 
 type tests = [
   Expect<Equal<SplitPath, ["Users", "John", "Documents", "notes.txt"]>>,
